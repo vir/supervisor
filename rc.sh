a5e5a8e4dd69ce47d0f8072e28609d3dd6260f21
Vasily Redkin <vir@ctm.ru>
2009-06-17 14:06:37 +0000

    buggy rc script

---
+++ rc.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+#
+# (c) vir
+#
+# Last modified: 2009-06-17 18:06:01 +0400
+#
+
+D=/home/vir/supervisor
+PIDFILE=$D/supervisor.PID
+
+start()
+{
+	sudo -u vir /bin/sh -c "$D/supervisor </dev/null >/dev/null 2>/dev/null &"
+	PID=$!
+	echo "$PID" > $PIDFILE
+}
+
+stop()
+{
+	echo "NOT WORKING"
+	rm $PIDFILE
+	exit 100
+	if [ -f $PIDFILE ]
+	then
+		PID=`head -1 $PIDFILE`
+		kill -INT -$PID
+	fi
+}
+
+check()
+{
+	if [ -f $PIDFILE ]
+	then
+		PID=`head -1 $PIDFILE`
+		return 0
+		return kill -0 $PID
+	else
+		return 1
+	fi
+}
+
+case $1 in
+	start)
+		if check
+		then
+			echo "Process $PID is (still) alive. Kill it or remove $PIDFILE"
+			exit 1
+		fi
+		start
+		;;
+	stop)
+		stop
+		;;
+	restart)
+		stop
+		start
+		;;
+	*)
+		echo "Usage: $0 start|stop|restart"
+		;;
+esac
+
+
