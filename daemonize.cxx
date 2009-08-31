63f1571b450d9e81dd93b1ac48cd818fbbb18680
Vasily Redkin <vir@ctm.ru>
2009-08-31 13:06:57 +0000

    Daemonization support (-D cmd-line option)

---
+++ daemonize.cxx
@@ -0,0 +1,35 @@
+#include "daemonize.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void daemonize()
+{
+	int pid, sid;
+
+	pid = fork();
+	if(pid > 0)
+		exit(0);
+	if(pid < 0) {
+		perror("fork");
+		exit(-1);
+	}
+
+	signal(SIGTSTP,SIG_IGN);
+	signal(SIGTTOU,SIG_IGN);
+	signal(SIGTTIN,SIG_IGN);
+	signal(SIGTERM,SIG_DFL);
+
+	sid = setsid();
+	if(sid < 0)
+		exit(-2);
+
+	chdir("/");
+
+	close(0);
+	close(1);
+	close(2);
+}
+
---
+++ daemonize.h
@@ -0,0 +1,15 @@
+#ifndef DAEMONIZE_H_INCLUDED
+#define DAEMONIZE_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void daemonize();
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* DAEMONIZE_H_INCLUDED */
+
