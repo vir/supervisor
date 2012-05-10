0876f65dfe09b66d04e7ae722b64639e567559c3
Vasily i. Redkin <vir@ctm.ru>
2012-05-10 11:05:57 +0400

    dded license (MIT)

--- daemonize.h
+++ daemonize.h
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2012 Vasily i. Redkin <vir@ctm.ru>
+ * License: MIT (See LICENSE.txt or http://www.opensource.org/licenses/MIT)
+ */
 #ifndef DAEMONIZE_H_INCLUDED
 #define DAEMONIZE_H_INCLUDED
 
