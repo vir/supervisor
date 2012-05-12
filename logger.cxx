ed0181912e4e69048f6239854905753366670554
User Vir <vir@R2D2.CTM.RU>
2012-05-12 15:34:56 +0400

    "log.microseconds" boolean config file opt. added

--- logger.cxx
+++ logger.cxx
@@ -8,7 +8,7 @@
 #include <sstream>
 
 Logger::Logger(const std::string & name)
-	:m_name(name), m_append_date(true)
+	:m_name(name), m_append_date(true), m_usecs_timestamps(false)
 {
 	m_filename = super.logdir() + "/" + m_name + ".log";
 	m_file.exceptions ( std::ofstream::eofbit | std::ofstream::failbit | std::ofstream::badbit );
@@ -20,21 +20,22 @@ Logger::~Logger()
 	m_file.close();
 }
 
-static std::string now()
+static std::string now(bool usecs = false)
 {
 	char outstr[200];
 	struct tm *tmp;
 	const static char * format = "%Y-%m-%d %H:%M:%S";
 	int len;
 	time_t t;
-
-#if 0
-	t = time(NULL);
-#else
 	struct timeval tv;
-	gettimeofday(&tv, NULL);
-	t = tv.tv_sec;
-#endif
+
+	if(usecs) {
+		gettimeofday(&tv, NULL);
+		t = tv.tv_sec;
+	} else {
+		t = time(NULL);
+	}
+
 	tmp = localtime(&t);
 	if (tmp == NULL) {
 		perror("localtime");
@@ -46,9 +47,10 @@ static std::string now()
 		fprintf(stderr, "strftime returned 0");
 		return "";
 	}
-#if 1
-	sprintf(outstr+len, ".%06ld", tv.tv_usec);
-#endif
+
+	if(usecs)
+		sprintf(outstr+len, ".%06ld", tv.tv_usec);
+
 	return std::string(outstr);
 }
 
@@ -57,7 +59,7 @@ std::string Logger::log(const std::string & source, std::string tag, const std::
 	std::ostringstream ss;
 //	std::cout << "Log[" << m_name << "](" << source << ", " << tag << ", " << msg << ", " << forcetimestamp << ")" << std::endl;
 	if(m_append_date || forcetimestamp)
-		ss << now() << " ";
+		ss << now(m_usecs_timestamps) << " ";
 	ss << source << "[" << tag << "] " << msg << std::endl;
 	if(!super.background())
 		std::cout << ss.str();
@@ -71,6 +73,9 @@ bool Logger::configure(const std::string & var, const std::string & value)
 	//std::cout << "Logger::configure(" << var << ", " << value << ")" << std::endl;
 	if(var == "timestamp") {
 		m_append_date = Config::to_bool(value);
+		return true;
+	} else if(var == "microseconds") {
+		m_usecs_timestamps = Config::to_bool(value);
 		return true;
 	}
 	return false;
